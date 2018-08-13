1be913171f850d4b5a6147dc8c8e59f48c34e7af
bsed <me@gnux.cn>
2018-08-13 13:46:23 +0800

    mini cssday wechat

---
+++ config.js
@@ -0,0 +1,9 @@
+var domain = "kelvin.mbioq.com"
+var name = "CSSDAY"
+var API_SECRET = "4e615a02e89a2webb358a7d31c3fbafd"
+
+export default {
+    getdomain: domain,
+    getname: name,
+    getapisecret: API_SECRET
+}
